1c49a6bd6a682ff9768e3d7cfa077211414025da
Spaghetti Jones <connor.breault@comcast.net>
2020-04-20 21:23:08 -0700

    band page responsive, about added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,10 +3,12 @@ var path = require("path");
 
 module.exports = function (app) {
 
+    // INDEX
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../views/index.html"));
     });
 
+    // MAIN PAGES
     app.get("/contact", function (req, res) {
         res.sendFile(path.join(__dirname, "../views/contact.html"));
     });
@@ -18,6 +20,12 @@ module.exports = function (app) {
     });
     app.get("/about", function (req, res) {
         res.sendFile(path.join(__dirname, "../views/about.html"));
+    });
+
+
+    // BANDS
+    app.get("/bands/smackdUp", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/smackdUp.html"));
     });
 
     // If no matching route is found default to home
