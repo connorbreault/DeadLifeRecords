a1e6fc344765404d3b2ae01572e800cc915c06f1
Spaghetti Jones <connor.breault@comcast.net>
2020-05-10 17:09:38 -0700

    band page filled with proper bands, bad html pages made and linked

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -21,7 +21,34 @@ module.exports = function (app) {
 
 
     // BANDS
+    app.get("/bands/apolloskey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/apolloskey.html"));
+    });
+    app.get("/bands/awakingmemory", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/awakingmemory.html"));
+    });
+    app.get("/bands/hardknox", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/hardknox.html"));
+    });
+    app.get("/bands/hauntedbyday", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/hauntedbyday.html"));
+    });
+    app.get("/bands/infinitointorqueo", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/infinitointorqueo.html"));
+    });
+    app.get("/bands/nailthecasket", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/nailthecasket.html"));
+    });
+    app.get("/bands/petroglyphs", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/petroglyphs.html"));
+    });
     app.get("/bands/smackdUp", function (req, res) {
         res.sendFile(path.join(__dirname, "../views/bands/smackdUp.html"));
+    });
+    app.get("/bands/shorelines", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/shorelines.html"));
+    });
+    app.get("/bands/thewolvesareclosingin", function (req, res) {
+        res.sendFile(path.join(__dirname, "../views/bands/thewolvesareclosingin.html"));
     });
 };
