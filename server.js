8f240fb9b9815db151b1c69f8530f03fd68099c2
Spaghetti Jones <connor.breault@comcast.net>
2020-10-09 17:13:07 -0700

    https enforced

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
 var express = require("express");
-var db = require("./models")
 var app = express();
+var db = require("./models");
 var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 // Routes
 require("./routes/htmlRoutes")(app);
@@ -19,18 +18,24 @@ var syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-    syncOptions.force = true;
+  syncOptions.force = true;
+} else if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-    app.listen(PORT, function () {
-        console.log(
-            "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-            PORT,
-            PORT
-        );
-    });
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
 
 module.exports = app;
